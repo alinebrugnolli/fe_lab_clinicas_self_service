8aafc36ab2844ac48ef243703b3b73a2a181add3
alinebrugnolli <aline.brugnolli@gmail.com>
2024-01-31 20:59:10 -0300

    Continuação da Construção do Auto Atendimento
    Continuação da Fase Starter - Splash Page
    Inicio da Fase Auth
    - Implementação do Repository - acesso camada de dados - acesso ao backend

---
+++ lib/src/binding/lab_clinicas_application_binding.dart
@@ -0,0 +1,12 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/core/env.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class LabClinicasApplicationBinding extends ApplicationBindings {
+  @override
+  List<Bind<Object>> bindings() => [
+        Bind.lazySingleton<RestClient>(
+          (i) => RestClient(Env.backendBaseUrl),
+        ),
+      ];
+}
