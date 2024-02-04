4342c192591e7f2233f534618071d1fb7c367e36
alinebrugnolli <aline.brugnolli@gmail.com>
2024-02-04 11:56:43 -0300

    Atualização do Auto Atendimento - Auto Atendimento.
    Criação da tela: find-patient
    Criado o arquivo find_patient_router.dart
    Estrutura de dados de tela para acesso ao backend
    Criamos os arquivos:
    patient_repository.dart
    patient_model.dart
    patient_address_model.dart
    E também Criamos uma nova coleção em Postman Patient
    Com duas novas requests:
    get Find Patient
    post Patient Register

--- lib/src/modules/self_service/find_patient/find_patient_page.dart
+++ lib/src/modules/self_service/find_patient/find_patient_page.dart
@@ -1,17 +1,129 @@
-import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:validatorless/validatorless.dart';
 
-class FindPatientPage extends StatelessWidget {
+class FindPatientPage extends StatefulWidget {
+  const FindPatientPage({super.key});
 
-  const FindPatientPage({ super.key });
+  @override
+  State<FindPatientPage> createState() => _FindPatientPageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-      Injector.get<SelfServiceController>().debug();
-       return Scaffold(
-           appBar: AppBar(title: const Text('Find Patient'),),
-           body: Container(),
-       );
+class _FindPatientPageState extends State<FindPatientPage> {
+  final formKey = GlobalKey<FormState>();
+  final documentEC = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: LabClinicasAppBar(
+        actions: [
+          PopupMenuButton(
+            child: const IconPopupMenuWidget(),
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem(
+                  value: 1,
+                  child: Text('Reiniciar Processo'),
+                ),
+              ];
+            },
+            onSelected: (value) async {
+              if (value == 1) {
+                final nav = Navigator.of(context);
+                await SharedPreferences.getInstance().then((sp) => sp.clear());
+                nav.pushNamedAndRemoveUntil('/', (route) => false);
+              }
+            },
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (_, constrains) {
+          var sizeOf = MediaQuery.sizeOf(context);
+          return SingleChildScrollView(
+            child: Container(
+              constraints: BoxConstraints(minHeight: constrains.maxHeight),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background_login.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(40),
+                  width: sizeOf.width * .8,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/images/logo_vertical.png'),
+                        const SizedBox(
+                          height: 48,
+                        ),
+                        TextFormField(
+                          controller: documentEC,
+                          validator: Validatorless.required('CPF Obrigatório'),
+                          decoration: const InputDecoration(
+                            label: Text('Digite o CPF do paciente'),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              'Não sabe o CPF do paciente',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: LabClinicasTheme.blueColor,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Clique aqui',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: LabClinicasTheme.orangeColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        SizedBox(
+                          width: sizeOf.width * .8,
+                          height: 48,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              final valid =
+                                  formKey.currentState?.validate() ?? false;
+                              if (valid) {}
+                            },
+                            child: const Text('CONTINUAR'),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/src/modules/self_service/self_service_module.dart
+++ lib/src/modules/self_service/self_service_module.dart
@@ -4,6 +4,7 @@ import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/documents/scan_confirm/documents_scan_confirm_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/done/done_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/find_patient/find_patient_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/find_patient/find_patient_router.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/patient/patient_page.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_page.dart';
@@ -27,13 +28,11 @@ class SelfServiceModule extends FlutterGetItModule{
   Map<String, WidgetBuilder> get pages => {
     '/':(context) => const SelfServicePage(),
     '/whoIAm':(context) => const WhoIAmPage(),
-    '/find-patient':(context) => const FindPatientPage(),
+    '/find-patient':(context) => const FindPatientRouter(),
     '/patient':(context) => const PatientPage(),
     '/documents':(context) => const DocumentsPage(),
     '/documents/scan':(context) => const DocumentsScanPage(),
     '/documents/scan/confirm':(context) => const DocumentsScanConfirmPage(),
-    '/done':(context) => const DonePage(),
-    
-  };
-  
+    '/done':(context) => const DonePage(),    
+  };  
 }
